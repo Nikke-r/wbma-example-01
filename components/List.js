e3e6eaecedc7e372c8039c2c7b774d39545f6ff7
Niklas Rönnholm <nikke.ronnholm@gmail.com>
2020-02-14 09:35:11 +0200

    Added a spinner to indicate loading to List component

--- components/List.js
+++ components/List.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 import React, {useContext} from 'react';
-import {List as BaseList} from 'native-base';
+import {List as BaseList, Spinner, Container} from 'native-base';
 import ListItem from './ListItem';
 import {MediaContext} from '../contexts/MediaContext';
 import {getAllMedia} from '../hooks/APIHooks';
@@ -9,14 +9,17 @@ import PropTypes from 'prop-types';
 const List = (props) => {
   const [media, setMedia] = useContext(MediaContext);
   const [data, loading] = getAllMedia();
-  console.log(loading);
   setMedia(data);
   return (
-    <BaseList
-      dataArray={media}
-      keyExtractor={(item, index) => index.toString()}
-      renderRow={(item) => <ListItem singleMedia={item} navigation={props.navigation} />}
-    />
+    <Container>
+      {loading ? <Spinner /> :
+          <BaseList
+            dataArray={media}
+            keyExtractor={(item, index) => index.toString()}
+            renderRow={(item) => <ListItem singleMedia={item} navigation={props.navigation} />}
+          />
+      }
+    </Container>
   );
 };
 
